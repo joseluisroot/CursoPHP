2de3392bacbda45e5bfc9d6d0ea7ca8c0a7532d0
Jose Luis Reyes <jreyes@smartcube.com.sv>
2014-05-04 12:03:58 -0600

    Examen
    Crear una funcion que calcule y muestre en una tabal los descuentos de
    AFP, ISS, RENTA para N empleados

--- clase4/finciones.php
+++ clase4/finciones.php
@@ -14,9 +14,50 @@ function operaciones($vector){
 	return $resultados;
 }
 
+//Crear una funcion que calcule y muestre en una tabal los descuentos de AFP, ISS, RENTA para N empleados
 
-$numero = array(1,2,3,4,5);
+function calcularRenta($Arreglo){
 
-var_dump(operaciones($numero));
+	$operar= array();
+
+	$calc= array();
+
+
+
+	foreach ($Arreglo as $indice => $value) {
+
+		 $calc= $value;
+		//renta*0.1
+		//afp*0.07
+		//isss*0.04
+		
+		$operar = ("empleado" => $value[0]);
+
+//echo $value[1]*0.04;
+//echo "<br>";
+		var_dump($operar);
+		//
+		
+		/*
+		,
+				   'Renta'=> $value[1]*0.1,
+				   'AFP'=> $value[1]*0.07,
+				   'ISSS'=> $value[1]*0.04
+		 */
+
+
+
+
+	}
+
+}
+
+
+
+$numero = array(['tolo',600],['emilia',100],['daniela',700]);
+
+calcularRenta($numero);
+
+//var_dump(operaciones($numero));
 
 ?>
